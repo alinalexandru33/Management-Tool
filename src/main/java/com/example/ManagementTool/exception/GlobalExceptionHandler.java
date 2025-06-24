67ea1d26cca0ae2369efba6ff85e16a0c05efaac
alinalexndru33 <alin.turcitu16@gmail.com>
2025-06-24 21:00:34 +0300

    Fixed a bug where Forbidden was not handled by our custom exception handler

--- src/main/java/com/example/ManagementTool/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/ManagementTool/exception/GlobalExceptionHandler.java
@@ -3,13 +3,22 @@ package com.example.ManagementTool.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
+import java.nio.file.AccessDeniedException;
+
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler({AccessDeniedException.class, AuthorizationDeniedException.class})
+    public ResponseEntity<String> handleAccessDenied(Exception ex) {
+        log.warn("Forbidden: {}", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ex.getMessage());
+    }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> handleNotFound(ResourceNotFoundException ex, WebRequest req) {
