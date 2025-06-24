2018f3c94f2fdae1cef05faeefc058329c938206
alinalexndru33 <alin.turcitu16@gmail.com>
2025-06-24 17:59:26 +0300

    Create entity & repository

---
+++ src/main/java/com/example/ManagementTool/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.example.ManagementTool.repository;
+
+import com.example.ManagementTool.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+    Optional<Product> findByName(String name);
+}
