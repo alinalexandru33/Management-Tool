14a3235d7f0058cae6faf0a8b6743b009cd30465
alinalexndru33 <alin.turcitu16@gmail.com>
2025-06-24 17:51:56 +0300

    Project initialization

---
+++ src/test/java/com/example/ManagementTool/ManagementToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ManagementTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
